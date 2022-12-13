4525be1500c2ddbf7228371ef620343a6afe8677
Fabio Forni <development@redaril.me>
2022-12-13 21:41:16 +0100

    Add compression level range

--- source/zstdd/core.d
+++ source/zstdd/core.d
@@ -3,12 +3,24 @@ module zstdd.core;
 import std.stdint;
 import std.string;
 
-uint32_t versionNumber() {
+uint32_t versionNumber() @trusted {
     extern (C) uint32_t ZSTD_versionNumber();
     return ZSTD_versionNumber();
 }
 
-char[] versionString() {
+char[] versionString() @trusted {
     extern (C) char* ZSTD_versionString();
     return std.string.fromStringz(ZSTD_versionString());
+}
+
+alias CompressionLevel = int32_t;
+
+CompressionLevel minCompressionLevel() @trusted {
+    extern (C) int32_t ZSTD_minCLevel();
+    return ZSTD_minCLevel();
+}
+
+CompressionLevel maxCompressionLevel() @trusted {
+    extern (C) int32_t ZSTD_maxCLevel();
+    return ZSTD_maxCLevel();
 }
