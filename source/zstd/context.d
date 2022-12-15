f80f08f3c93ad41388009d327785e339c8211cae
Fabio Forni <development@redaril.me>
2022-12-15 14:30:18 +0100

    Add CompressionContext.setPledgedSrcSize()

--- source/zstd/context.d
+++ source/zstd/context.d
@@ -1,5 +1,7 @@
 module zstd.context;
 
+import std.stdint;
+
 import zstd.c.symbols;
 import zstd.simple;
 public import zstd.c.typedefs : Strategy, CompressionParameter, Bounds;
@@ -29,6 +31,15 @@ class CompressionContext
     void setParameter(CompressionParameter param, int value)
     {
         const auto errCode = ZSTD_CCtx_setParameter(ptr, param, value);
+        if (ZSTD_isError(errCode))
+        {
+            throw new ZSTDException(errCode);
+        }
+    }
+
+    void setPledgedSrcSize(uint64_t pledgedSrcSize)
+    {
+        const auto errCode = ZSTD_CCtx_setPledgedSrcSize(ptr, pledgedSrcSize);
         if (ZSTD_isError(errCode))
         {
             throw new ZSTDException(errCode);
