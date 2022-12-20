c280dc8792a4571b4af341bf78f50183bef43818
Fabio Forni <development@redaril.me>
2022-12-20 18:54:50 +0100

    Make tests buildable with meson

--- source/zstd/package.d
+++ source/zstd/package.d
@@ -4,3 +4,14 @@ public import zstd.common;
 public import zstd.context;
 public import zstd.dict;
 public import zstd.func;
+
+/* Meson doesn't automatically add a main function, unlike dub. */
+version (unittest)
+{
+    debug (meson)
+    {
+        void main()
+        {
+        }
+    }
+}
