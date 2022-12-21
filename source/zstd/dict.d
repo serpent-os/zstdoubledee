d2fdb4abf54cc9c5400788e5e52561a4d7e91aa8
Fabio Forni <development@redaril.me>
2022-12-21 13:59:10 +0100

    dict.d: Add documentation

--- source/zstd/dict.d
+++ source/zstd/dict.d
@@ -5,8 +5,14 @@ import std.stdint;
 import zstd.c.symbols;
 import zstd.common;
 
+/**
+ * Wraps [ZSTD_CDict_s]. Resorces are managed internally and are automatically freed.
+ */
 class CompressionDict
 {
+    /**
+     * Wraps [ZSTD_createCDict].
+     */
     this(const void[] dictBuffer, CompressionLevel lvl)
     {
         ptr = ZSTD_createCDict(dictBuffer.ptr, dictBuffer.length, lvl);
@@ -17,11 +23,17 @@ class CompressionDict
         ZSTD_freeCDict(ptr);
     }
 
+    /**
+     * Wraps [ZSTD_getDictID_fromCDict].
+     */
     uint32_t getDictID()
     {
         return ZSTD_getDictID_fromCDict(ptr);
     }
 
+    /**
+     * Wraps [ZSTD_sizeof_CDict].
+     */
     size_t sizeOf()
     {
         return ZSTD_sizeof_CDict(ptr);
@@ -41,8 +53,14 @@ unittest
     assert(new CompressionDict(null, 1).sizeOf() > 0);
 }
 
+/**
+ * Wraps [ZSTD_DDict]. Resorces are managed internally and are automatically freed.
+ */
 class DecompressionDict
 {
+    /**
+     * Wraps [ZSTD_createDDict].
+     */
     this(const void[] dictBuffer)
     {
         ptr = ZSTD_createDDict(dictBuffer.ptr, dictBuffer.length);
@@ -53,11 +71,17 @@ class DecompressionDict
         ZSTD_freeDDict(ptr);
     }
 
+    /**
+     * Wraps [ZSTD_getDictID_fromDDict].
+     */
     uint32_t getDictID()
     {
         return ZSTD_getDictID_fromDDict(ptr);
     }
 
+    /**
+     * Wraps [ZSTD_sizeof_DDict].
+     */
     size_t sizeOf()
     {
         return ZSTD_sizeof_DDict(ptr);
